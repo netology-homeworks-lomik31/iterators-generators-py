851bb0bbd262c96206cdc7b08f41d7fb9fdf75eb
lomik31 <lomik31@codead.dev>
2023-08-09 01:23:17 +0300

    Solution of fourth task

--- 4.py
+++ 4.py
@@ -1,10 +1,18 @@
 import types
 
+def unpack_list(list_of_list):
+    lst = []
+    for i in list_of_list:
+        if isinstance(i, list): lst += i
+        else: lst.append(i)
+    for i in lst:
+        if isinstance(i, list): return flat_generator(lst)
+    return lst
 
 def flat_generator(list_of_list):
-    ...
-    yield
-    ...
+    lst = unpack_list(list_of_list)
+    for i in lst:
+        yield i
 
 def test_4():
 
